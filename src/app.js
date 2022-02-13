b31f29f43785172940efd53fd514acf52b982bb3
JulianVera <veeraa1270@gmail.com>
2022-02-13 02:49:06 +0100

    npm run start

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-//npm start
+//npm run start
 app.listen(process.env.PORT || 3030, () => console.log('Servidor corriendo'))
 
 //pagina home
