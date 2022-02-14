3095849ed31a26514207e3b6535f53ab3f19e9a2
felipe1120gomez <felipe92agudelo@gmail.com>
2022-02-14 00:01:17 -0500

    Add relativePath for checker in productControllers

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -1,10 +1,13 @@
 const fs = require('fs')
+const path = require('path')
 
 let productsController = {
-    detail: function(req, res) { /* SE ESPECIFICA LA RUTA /products/deatil/productid */
-        let prodId = req.params.productid
-        let prodPath = `./products/${prodId}`
-        if (fs.existsSync(prodPath)) {
+    detail: function(req, res) {
+        let prodId = req.params.productid /* id del producto */
+        let prodPath = `./products/${prodId}` /* ruta a renderizar */
+        let relativePath = `../views/${prodPath}.ejs` /* ruta relativa a comprobar */
+        /* si la ruta relativa existe (si el archivo existe), se renderiza */
+        if (fs.existsSync(path.join(__dirname, relativePath))) {
             res.render(prodPath)
         } else {
             res.render('error')
