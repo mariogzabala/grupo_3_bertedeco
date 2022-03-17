2e65380676863e3aeee710b42b3b77a39884bc1d
felipe1120gomez <felipe92agudelo@gmail.com>
2022-03-17 08:47:10 -0500

    Users config for sprint 5

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,13 +1,57 @@
+const fs = require('fs')
+const path = require('path')
+
+/* Donde esta el JSON */
+const usersFilePath = path.join(__dirname, '../database/usersDataBase.json')
+
+/* Donde se guardan las imagenes */
+const storepath = path.join(__dirname, '../../public/img/users/')
+
+/* Los tipos de archivos aceptados */
+const imgList = ['image/png', 'image/jpeg']
+
+/* POR FAVOR ESCRIBAN CODIGO BIEN BONITO Y COMENTARIOS */
+
+let users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8')) /* usen esto donde lo necesiten */
+
 let userController = {
-    login: function(req, res) { /* SE ESPECIFICA LA RUTA /users/register */
+    login: function(req, res) {
+        /* Mostrar el formulario de login */
         res.render('./users/login')
     },
 
-    register: function(req, res) { /* SE ESPECIFICA LA RUTA /users/register */
+    authentication: function(req, res) {
+        /* Recibe el email y el password por body
+        Se busca en el json el email, si se encuentra se
+        Comparan las contraseñas, si coinciden se da acceso,
+        De lo contrario se devuelve un mensaje indicando el error.
+        Al acceder se redirecciona a un pagina con un mensaje de exito
+        desde donde se puede acceder al resto del sitio web
+        TENER EN CUENTA LO DE LAS COOKIES O SESSION*/
+    },
+
+    register: function(req, res) {
+        /* Mostrar el formulario de registro */
         res.render('./users/register')
     },
 
-    profile: function(req, res) { /* SE ESPECIFICA LA RUTA /users/register */
+    store: function(req, res) {
+        /* Hacer practicamente todo el proceso de crear el usario imitando
+        lo que se hizo en crear producto. Tener en cuenta no crear un usario si
+        ya esta registrado el email, devolver un mensaje de error indicando esto
+        cifrar la contraseña, hacer el proceso de las cookies o session
+        si se tiene exito redireccionar a la pagina de perfil del usuario */
+    },
+
+    profile: function(req, res) {
+        /* Mostrar el perfil del usuario */
+        let users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'))
+        res.render('./users/profile')
+    },
+
+    edit: function(req, res) {
+        /* editar el perfil del usuario */
+        let users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'))
         res.render('./users/profile')
     }
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,10 +3,27 @@ const router = express.Router()
 
 const usersController = require('../controllers/usersControllers.js')
 
+/* PUEDEN AGREGAR MIDELWARES COMO VALIDATOR SI LO DESEAN
+TAMBIEN CREAR MODULOS PARA COOKIES O SESSION EN LA CARPETA modules */
+
+/* POR FAVOR ESCRIBAN CODIGO BIEN BONITO Y COMENTARIOS */
+
+/* Mostrar login page*/
 router.get('/login', usersController.login)
 
+/* Enviar y procesar email y password para login */
+router.post('/auth', usersController.authentication)
+
+/* Mostrar formulario de registro*/
 router.get('/register', usersController.register)
 
-router.get('/profile', usersController.profile)
+/* Guardar usuario */
+router.post('/register', usersController.store)
+
+/* Mostrar perfil del usuario*/
+router.get('/profile/:id?', usersController.profile) /* Borrar el ? */
+
+/* Editar perfil del usuario*/
+router.post('/profile/edit/:id', usersController.edit)
 
 module.exports = router
